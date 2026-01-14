52310341760261456f3b95efaf97fef6f0ffb4db
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-01-14 12:26:18 +0100

    nevnap feladat kész

--- app/Http/Controllers/NevnapController.php
+++ app/Http/Controllers/NevnapController.php
@@ -2,20 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Nevnap;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class NevnapController extends Controller
 {
-    // /nevnapok?nev={} VAGY /nevnapok?nap={}
+    // /nevnapok?nev={} VAGY /nevnapok?nap={1-2}
 
     public function index(Request $request){
         if ($request->has('nap'))
         {
-            
+            [$ho,$nap] = explode('-',$request->nap);
+
+            // Log::info($ho . "-" . $nap);
+
+            $adat = Nevnap::where('ho',$ho)->where('nap',$nap)->get();
+        
+            return response()->json($adat,200,options:JSON_UNESCAPED_UNICODE);
+
         }
         else if ($request->has('nev')) 
         {
-            
+            $nev = $request->nev;
+            $adat = Nevnap::where('nev1',$nev)->orWhere('nev2',$nev)->get();
+
+            return response()->json($adat,200,options:JSON_UNESCAPED_UNICODE);
         }
         else
         {
